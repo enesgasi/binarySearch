d5998f2a58d27e972589b386d2dd7c7de75fdd0f
enesgasi <enesgasi@hotmail.com>
2023-05-26 10:00:45 +0300

    first

---
+++ main.js
@@ -0,0 +1,28 @@
+const arr = ['a','b','c','x','y','z'];
+
+function findMe(target, start, end){
+
+    if (start > end){
+        return 'Not found';
+    }
+
+    const middle = Math.floor( (start+end) / 2 );
+
+    if (arr[middle] === target) {
+
+        `return Found it at index ${middle}`
+    }
+
+    if (arr[middle] > target) {
+        return findMe(target, start, middle-1);
+    }
+
+    if(arr[middle] < target){
+        return findMe(target, middle+1, end);
+    }
+
+}
+
+output = findMe(50, 1, 500);
+
+console.log(output);
